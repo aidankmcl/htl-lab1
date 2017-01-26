4288ee5d7ef4f4d957a83001ab28ead04fc0dcf3
Aidan <aidankmcl@gmail.com>
2017-01-25 22:38:09 -0500

    some initial edits

--- server.py
+++ server.py
@@ -15,17 +15,30 @@ app = Flask(__name__)
 
 courses = pd.read_csv('./data/olin-courses-16-17.csv')
 
+
 @app.route('/health')
 def health():
     return 'ok'
 
+
 @app.route('/')
 def home_page():
-    return render_template('index.html', areas=set(courses.course_area), contacts=set(courses.course_contact.dropna()))
+    areas = set(courses.course_area)
+    instructors = {}
+    for area in areas:
+        area_instructors = set([course[1].course_contact for course in courses[courses.course_area == area].iterrows()])
+        instructors[area] = area_instructors
+    return render_template('index.html', areas=areas, course_instructors=instructors)
+
 
 @app.route('/area/<course_area>')
 def area_page(course_area):
-    return render_template('course_area.html', courses=courses[courses.course_area == course_area].iterrows())
+    return render_template('courses.html', areas=set(courses.course_area), courses=courses[courses.course_area == course_area].iterrows())
+
+
+@app.route('/instructor/<instructor>')
+def instructor_page(instructor):
+    return render_template('courses.html', areas=set(courses.course_area), courses=courses[courses.course_contact == instructor].iterrows(), instructor=instructor)
 
 if __name__ == '__main__':
     app.run(debug=True)
