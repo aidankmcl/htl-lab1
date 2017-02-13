f14e86e8b65cc7a43a54fe566bce4b16bf6dd3d3
Aidan <aidankmcl@gmail.com>
2017-02-13 10:39:46 -0500

    changed host according to environment

--- server.py
+++ server.py
@@ -42,4 +42,5 @@ def instructor_page(instructor):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', debug=True, port=port)
+    host = os.environ.get('HOST', '127.0.0.1')
+    app.run(host=host, debug=True, port=port)
